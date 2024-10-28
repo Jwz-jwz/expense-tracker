b9374658394681d367d8b7f2c505e636eb16cd89
Жавзандулам Санжаасүрэн <24LP4023@G401-CC0100.local>
2024-10-28 12:45:35 +0800

    fsdf

--- back-end/server.js
+++ back-end/server.js
@@ -1,83 +1,56 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import fs from "fs";
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 8888;
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
+
+const port = 8368;
+const sql = neon(`${process.env.DATABASE_URL}`);
+
 app.get("/", (request, response) => {
   response.send("hello tanii GET huselt irlee");
 });
 
-app.post("/", (request, response) => {
-  const { name, password } = request.body;
+app.post("/sign-in", async (request, response) => {
+  const { email, password } = request.body;
 
-  fs.readFile("./data/user.json", "utf-8", (readError, data) => {
-    if (readError) {
-      response.json({
-        success: false,
-        error: error,
-      });
+  try {
+    const user = await sql`SELECT * FROM users WHERE email=${email}`;
+    if (user.length === 0) {
+      return response.json({ message: "email or password not match" });
     }
 
-    let savedData = data ? JSON.parse(data) : [];
-
-    const registeredUser = savedData.filter(
-      (user) => user.name === name && user.password === password
-    );
-
-    if (registeredUser.length > 0) {
-      response.json({
-        success: true,
-        user: registeredUser[0],
-      });
-    } else {
-      response.json({
-        success: false,
-      });
+    if (user[0].password !== password) {
+      return response.json({ message: "password not match" });
     }
-  });
+
+    response.json({ message: "Login successfull", user: user[0] });
+  } catch (error) {
+    response.json({ message: "Internal server error during login user" });
+  }
 });
-
-app.post("/sign-up-page", (request, response) => {
+app.post("/sign-up", async (request, response) => {
   const { name, email, password } = request.body;
-  fs.readFile("./data/user.json", "utf-8", (readError, data) => {
-    let savedData = data ? JSON.parse(data) : [];
-
-    if (readError) {
-      response.json({
-        success: false,
-        error: error,
-      });
+  //
+  try {
+    const existedUser = await sql`SELECT * FROM users WHERE email=${email}`;
+    if (existedUser.length > 0) {
+      return response.json({ message: "User already exist" });
     }
+    const newUser = await sql`INSERT INTO users (email, name, password)
+                VALUES(${email}, ${name}, ${password} )
+                RETURNING id, email`;
 
-    console.log(data);
-
-    const newUser = {
-      id: Date.now().toString(),
-      name: name,
-      email: email,
-      password: password,
-    };
-    savedData.push(newUser);
-
-    fs.writeFile("./data/user.json", JSON.stringify(savedData), (error) => {
-      if (error) {
-        response.json({
-          success: false,
-          error: error,
-        });
-      } else {
-        response.json({
-          success: true,
-          user: newUser,
-        });
-      }
-    });
-  });
+    response.json({ message: "User successfully created", user: newUser[0] });
+    console.log(response);
+  } catch (error) {
+    response.json({ message: "Internal server error during login user" });
+  }
 });
 
 app.listen(port, () => {
